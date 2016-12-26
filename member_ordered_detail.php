a59e8d14347dd4d87c201ef683584efd02774224
吳孟庭 <b98602046@ntu.edu.tw>
2016-12-27 00:13:06 +0800

    會員中心

--- member_ordered_detail.php
+++ member_ordered_detail.php
@@ -21,10 +21,7 @@ $result = $mysqli->query($sql);
     <link href="css/login_main.css" rel="stylesheet" type="text/css">
 
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-    <!-- bootstrap -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link href="source/css/bootstrap.min.css" rel="stylesheet">
-    <script src="source/js/bootstrap.min.js"></script>
+
 
 
     <!-- Icons -->
@@ -87,17 +84,17 @@ $result = $mysqli->query($sql);
             <nav>
                 <ul>
                     <!-- 當前頁面掛上here的class -->
-                    <li class="icon_aboutus ">
+                    <li class="icon_aboutus">
                         <a href="aboutus.html"></a>
                     </li>
                     <li class="icon_product">
                         <a href="product.html"></a>
                     </li>
                     <li class="icon_custom">
-                        <a href=""></a>
+                        <a href="custom.html"></a>
                     </li>
                     <li class="icon_inspire">
-                        <a href=""></a>
+                        <a href="inspire.html"></a>
                     </li>
                 </ul>
             </nav>
@@ -115,7 +112,9 @@ $result = $mysqli->query($sql);
 
 
     <content>
-        <div class="con">
+
+
+        <div class="con3">
             <!-- 麵包屑 -->
             <div class="loginnav">
                 <p>ACCOUNT-MEMBER</p>
@@ -132,42 +131,44 @@ $result = $mysqli->query($sql);
                 </div>
                 <div class="up3 here">
                     <img src="images/member/mushroom.svg">
-                    <a href="">訂單查詢</a>
+                    <a href="member_ordered_list.php">訂單查詢</a>
                 </div>
             </div>
 
             <!-- 下方欄位 -->
-            <div class="downframe">
+            <div class="downframe2">
                 <p>ORDERS 訂單查詢</p>
                 <img src="images/member/line2-01.svg">
             </div>
+
             <?php
             while ($row = $result->fetch_assoc()): ?>
             <div class="member-content clearfix">
                 <div class="member-content-hd">
-                    <h4>訂單編號：PO20161116073692</h4>
+                    <h4>▍ 訂單編號：PO20161116073</h4>
                 </div>
+
                 <div class="member-content-bd">
                     <div class="section">
                         <table class="table-order table-order--detail">
                             <tbody>
-                            <tr>
-                                <th width="30%">商品名稱</th>
-                                <th>數量</th>
-                                <th>單價</th>
-                                <th>折扣</th>
-                                <th>規格</th>
-                                <th>小計</th>
-                            </tr>
-                            <tr>
-                                <td><?=$row['sid']?></td>
-                                <td>1</td>
-                                <td><?=$row['amount']?></td>
-                                <td>0</td>
-                                <td><?=$row['amount']?></td>
-                                <td><?=$row['amount']?></td>
+                                <tr>
+                                    <th width="30%">商品名稱</th>
+                                    <th>數量</th>
+                                    <th>單價</th>
+                                    <th>折扣</th>
+                                    <th>規格</th>
+                                    <th>小計</th>
+                                </tr>
+                                <tr>
+                                    <td><?=$row['sid']?></td>
+                                    <td>1</td>
+                                    <td><?=$row['amount']?></td>
+                                    <td>0</td>
+                                    <td><?=$row['amount']?></td>
+                                    <td><?=$row['amount']?></td>
 
-                            </tr>
+                                </tr>
                             </tbody>
                         </table>
                         <!-- <div class="cart-bill">
@@ -201,75 +202,70 @@ $result = $mysqli->query($sql);
                         <h5>訂購資料</h5>
                         <table class="table-order">
                             <tbody>
-                            <tr>
-                                <th>訂購日期</th>
-                                <th>收件人</th>
-                                <th>出貨日期</th>
-                                <th>包裹編號</th>
-                                <th width="25%">備註</th>
-                            </tr>
-                            <tr>
-                                <td><?=$row['order_date']?></td>
-                                <td><?=$row['nickname']?></td>
-                                <td><?=$row['order_date']?></td>
-                                <td>15710309</td>
-                                <td>已出貨</td>
-                            </tr>
+                                <tr>
+                                    <th>訂購日期</th>
+                                    <th>收件人</th>
+                                    <th>出貨日期</th>
+                                    <th>包裹編號</th>
+                                    <th width="25%">備註</th>
+                                </tr>
+                                <tr>
+                                    <td><?=$row['order_date']?></td>
+                                    <td><?=$row['nickname']?></td>
+                                    <td><?=$row['order_date']?></td>
+                                    <td>15710309</td>
+                                    <td>已出貨</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
 
                     <div class="section section-order-detail">
                         <h5>付款及運送方式</h5>
-                        <table class="table-order">
-                            <tbody>
-                            <tr>
-                                <th>付款方式</th>
-                                <th>付款結果</th>
-                                <th>運送方式</th>
-                                <th>運送地址</th>
-                            </tr>
-                            <tr>
-                                <td>7-11 門市付款</td>
-                                <td><!-- 完成付款 --> </td>
-                                <td><!-- 7-11 (蘆三門市) --></td>
-                                <td><!-- 新北市蘆洲區三民路6號8號 --></td>
-                            </tr>
-                            </tbody>
-                        </table>
+                            <table class="table-order">
+                                <tbody>
+                                    <tr>
+                                        <th>付款方式</th>
+                                        <th>付款結果</th>
+                                        <th>運送方式</th>
+                                        <th>運送地址</th>
+                                    </tr>
+                                    <tr>
+                                        <td>7-11 門市付款</td>
+                                        <td><!-- 完成付款 --> </td>
+                                        <td><!-- 7-11 (蘆三門市) --></td>
+                                        <td><!-- 新北市蘆洲區三民路6號8號 --></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                     </div>
 
                     <div class="section section-order-detail">
                         <h5>發票資訊</h5>
                         <table class="table-order">
                             <tbody>
-                            <tr>
-                                <th>發票種類</th>
-                                <!-- <th>手機載具條碼</th>
-                                <th>愛心碼</th>
-                                <th>統一編號</th>
-                                <th>抬頭</th> -->
-                            </tr>
-                            <tr>
-                                <td>電子發票</td>
-                                <!-- <td><span>手機載具條碼</span>---</td>
-                                <td><span>愛心碼</span>---</td>
-                                <td><span>統一編號</span>---</td>
-                                <td><span>抬頭</span>---</td> -->
-                            </tr>
+                                <tr>
+                                    <th>發票種類</th>
+                                    <!-- <th>手機載具條碼</th>
+                                    <th>愛心碼</th>
+                                    <th>統一編號</th>
+                                    <th>抬頭</th> -->
+                                </tr>
+                                <tr>
+                                    <td>電子發票</td>
+                                    <!-- <td><span>手機載具條碼</span>---</td>
+                                    <td><span>愛心碼</span>---</td>
+                                    <td><span>統一編號</span>---</td>
+                                    <td><span>抬頭</span>---</td> -->
+                                </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-            <hr>
+            
             <?php endwhile; ?>
-            <!-- <div class="ordernum">
-                <p>訂單編號</p>
-                <p>PO20161116073692</p>
-            </div> -->
-
-
+            
         </div>
 
 
